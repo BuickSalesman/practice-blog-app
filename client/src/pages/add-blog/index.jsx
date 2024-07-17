14075fa54c5a17c72b36a9d5f3e1638d16ad0a8f
BuickSalesman <adam.odonnell.e@gmail.com>
2024-07-16 21:54:30 -0400

    created form and form data, working on onclick methods

--- client/src/pages/add-blog/index.jsx
+++ client/src/pages/add-blog/index.jsx
@@ -1,7 +1,43 @@
+import { useContext } from "react";
+import classes from "./styles.module.css";
+import { GlobalContext } from "../../context";
+
 export default function AddNewBlog() {
+  const { formData, setFormData } = useContext(GlobalContext);
+
+  console.log(formData);
+
   return (
-    <div>
+    <div className={classes.wrapper}>
       <h1>Add Blog</h1>
+      <div className={classes.formWrapper}>
+        <input
+          name="title"
+          placeholder="Enter Blog Title"
+          id="title"
+          type="text"
+          value={formData.title}
+          onChange={(event) =>
+            setFormData({
+              ...formData,
+              title: event.target.value,
+            })
+          }
+        />
+        <textarea
+          name="description"
+          placeholder="Enter Blog Description"
+          id="description"
+          value={formData.description}
+          onChange={(event) =>
+            setFormData({
+              ...formData,
+              description: event.target.value,
+            })
+          }
+        ></textarea>
+        <button onClick={handleSavebBlogToDatabase}>Add Blog</button>
+      </div>
     </div>
   );
 }
