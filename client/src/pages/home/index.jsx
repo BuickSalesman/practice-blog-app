0e19faf98cff75b0d1ddc5df122b28a447c52f6d
BuickSalesman <adam.odonnell.e@gmail.com>
2024-07-16 23:19:57 -0400

    fixed delete error

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -26,7 +26,7 @@ export default function Home() {
   }
 
   async function handleDeleteBlog(getCurrentId) {
-    const response = await axios.delete(`http://localhost5000/api/blogs/delete/${getCurrentId}`);
+    const response = await axios.delete(`http://localhost:5000/api/blogs/delete/${getCurrentId}`);
     const result = await response.data;
 
     if (result?.message) {
